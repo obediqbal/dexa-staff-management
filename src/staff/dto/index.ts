b933fc1bb46e6242124d28dd114ded94485d945a
Rezapu Obed <obed.ubaidillah@gmail.com>
2026-01-03 21:01:45 +0700

    chore: rename dto

--- src/staff/dto/index.ts
+++ src/staff/dto/index.ts
@@ -1,3 +1,3 @@
 export { CreateStaffDto } from './create-staff.dto';
 export { UpdateStaffDto } from './update-staff.dto';
-export { PaginationDto } from './pagination.dto';
+export { FindAllDto } from './find-all.dto';
